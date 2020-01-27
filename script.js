c771cfdeb20c9c8420afdf9f436d6ae558533642
Kelly Bazella <kelly.bazella@gmail.com>
2020-01-26 19:27:41 -0600

    need to add date to 5 day forecast and search history

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 //global variables
 var apiKey = "6c8bace821ea4c6237ba931d1ab4ccf9"
+
 // all functions
 
 //weather function
@@ -10,21 +11,22 @@ function weatherApi(city) {
         method: "GET"
     }).then(function (response) {
         console.log(response)
-        $(".temp").text(response.main.temp+"°F");
+        $(".temp").text(response.main.temp + "°F");
         $(".humidity").text(response.main.humidity);
         $(".wind-speed").text(response.wind.speed);
         uvIndex(response.coord.lat, response.coord.lon);
     })
 }
 //forecast
-function forecastApi(city){
+
+function forecastApi(city) {
     var queryURL = " http://api.openweathermap.org/data/2.5/forecast?units=imperial&q=" + city + "&APPID=" + apiKey;
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function (response) {
-        var forecast = response.list.filter(function(element){
-            return(element.dt_txt.split(" ")[1]==="15:00:00")
+        var forecast = response.list.filter(function (element) {
+            return (element.dt_txt.split(" ")[1] === "15:00:00")
         });
         console.log(forecast);
         $(".temp1").text(forecast[0].main.temp);
@@ -38,13 +40,38 @@ function forecastApi(city){
         $(".humidity4").text(forecast[3].main.humidity);
         $(".humidity5").text(forecast[4].main.humidity);
         // console.log(forecast[0].weather[0].icon)
-        // var iconURL = "http://openweathermap.org/img/w/"+iconCode+".png";
-        // var iconCode = forecast[0].weather[0].icon;
-        // $(".icon1").attr("src",iconURL);
+        var iconCode1 = forecast[0].weather[0].icon;
+        var iconCode2 = forecast[1].weather[0].icon;
+        var iconCode3 = forecast[2].weather[0].icon;
+        var iconCode4 = forecast[3].weather[0].icon;
+        var iconCode5 = forecast[4].weather[0].icon;
+        var iconURL1 = "http://openweathermap.org/img/w/"+iconCode1+".png";
+        var iconURL2 = "http://openweathermap.org/img/w/"+iconCode2+".png";
+        var iconURL3 = "http://openweathermap.org/img/w/"+iconCode3+".png";
+        var iconURL4 = "http://openweathermap.org/img/w/"+iconCode4+".png";
+        var iconURL5 = "http://openweathermap.org/img/w/"+iconCode5+".png";
+        $(".icon1").attr("src",iconURL1);
+        $(".icon2").attr("src",iconURL2);
+        $(".icon3").attr("src",iconURL3);
+        $(".icon4").attr("src",iconURL4);
+        $(".icon5").attr("src",iconURL5);
     })
-    $(".data-input").val("")
-}
 
+    $(".data-input").val("");
+
+}
+function forecastDate(city){
+    var queryURL = " http://api.openweathermap.org/data/2.5/forecast?units=imperial&q=" + city + "&APPID=" + apiKey;
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        var forecast = response.list.filter(function (element) {
+            return (element.dt_txt.split(" ")[0])
+        });
+        console.log(forecast);
+})
+}
 // function weatherIcon (iconCode){
 //     for (var i = 0; i<=4;i++){
 //         var iconCode = forecast[i].weather[0].icon
@@ -58,20 +85,30 @@ function forecastApi(city){
 //             return(element.dt_txt.split(" ")[1]==="15:00:00")
 //             $(".icon1").attr("src",iconCode)
 //         }); 
-        
+
 //     })
 // }
 
 //uv function
-function uvIndex(lat, lon){
-    var queryURL = "http://api.openweathermap.org/data/2.5/uvi?appid=" + apiKey + "&lat="+lat+ "&lon="+lon;
+function uvIndex(lat, lon) {
+    var queryURL = "http://api.openweathermap.org/data/2.5/uvi?appid=" + apiKey + "&lat=" + lat + "&lon=" + lon;
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function (response) {
         console.log(response);
-        $(".uv-index").text(response.value)
-        $(".city-name").text(userInput)
+        var uvIndex = response.value
+        $(".uv-index").text(uvIndex)
+
+        if (uvIndex <= 3) {
+            $(".uv-index").css("background-color", "green")
+        }
+        else if ((uvIndex > 4) && (uvIndex <= 7)) {
+            $(".uv-index").css("background-color", "yellow")
+        }
+        else if (uvIndex > 8) {
+            $(".uv-index").css("background-color", "red")
+        }
     })
 }
 //click events
